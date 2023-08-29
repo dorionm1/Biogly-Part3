bf7bffe14b6146795687bfef31cb5b2897a3c04d
Dorion Mitchell <dorionmitchell@Dorions-MBP.local>
2023-08-28 21:58:35 -0400

    added new feratures for part 2

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 """Blogly application."""
 from flask import Flask, render_template, request, redirect, jsonify
-from models import db, connect_db, User
+from models import db, connect_db, User, Post
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
@@ -16,14 +16,24 @@ def home():
     return render_template('home.html')
 
 @app.route('/user-create')
-def show_form():
+def show_create_form():
     user = User
     return render_template('user-create.html', user=user)
+
+@app.route('/post-create/<int:id>')
+def show_post_form(id):
+    user = User.query.get(id)
+    return render_template('post-create.html', user=user)
 
 @app.route('/user-edit/<int:id>')
 def edit_user_page(id):
     user = User.query.get(id)
     return render_template('user-edit.html', user=user)
+
+@app.route('/post-edit/<int:id>')
+def edit_user_post(id):
+    post = Post.query.get(id)
+    return render_template('post-edit.html', post=post)
 
 @app.route('/edit-complete/<int:id>', methods=['POST'])
 def edit_user_task(id):
@@ -40,6 +50,19 @@ def edit_user_task(id):
 
     return redirect(f'/{id}')
 
+@app.route('/edit-post-complete/<int:id>', methods=['POST'])
+def edit_post_task(id):
+    post = Post.query.get(id)
+    new_title = request.form.get('title')
+    new_content = request.form.get('content')
+
+    post.title = new_title
+    post.content = new_content
+
+    db.session.commit()
+
+    return redirect(f'/post/{id}')
+
 @app.route('/user-list')
 def show_list():
     """Shows list of all users in db"""
@@ -47,10 +70,18 @@ def show_list():
     return render_template('user-list.html', users=users)
 
 @app.route('/<int:id>')
-def show_detail(id):
+def show_user_detail(id):
     """Show details of a single user"""
     user = User.query.get(id)
-    return render_template('user-detail.html', user=user)
+    posts = Post.query.filter_by(user_id=id)
+
+    return render_template('user-detail.html', user=user, posts=posts)
+
+@app.route('/post/<int:id>')
+def show_post_detail(id):
+    post = Post.query.get(id)
+
+    return render_template('post-detail.html', post=post)  
 
 @app.route('/user-detail', methods=['POST'])
 def create_user():
@@ -59,13 +90,26 @@ def create_user():
     image_url = request.form["image_url"]
 
     new_user = User(first_name=first_name, last_name=last_name, image_url=image_url)
+
     db.session.add(new_user)
     db.session.commit()
 
-    users = User.query.all()
+    users = User.query.all()    
     latest_id= users[-1].id
 
     return redirect(f'/{latest_id}')
+
+@app.route('/create-post/<int:id>', methods=['POST'])
+def create_post(id):
+    user = User.query.get(id)
+    title = request.form["title"]
+    content = request.form["content"]
+
+    new_post = Post(user=user,title=title, content=content)
+    db.session.add(new_post)
+    db.session.commit()
+
+    return redirect("/user-list")
 
 @app.route('/remove-user/<int:id>')
 def remove_user(id):
--- models.py
+++ models.py
@@ -1,4 +1,5 @@
 from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 db = SQLAlchemy()
 
@@ -25,3 +26,21 @@ class User(db.Model):
     image_url = db.Column(db.String,
                           nullable=False)
     
+class Post(db.Model):
+    def __repr__(self):
+        """Show info about user."""
+        u = self
+        return f"<Post {u.id} {u.title} {u.content} {u.created_at} {u.user_id}>"
+    
+    __tablename__ = "post"
+
+    id = db.Column(db.Integer,
+                   primary_key=True,
+                   autoincrement=True)
+    title = db.Column(db.String, nullable=False)
+    content = db.Column(db.String, nullable=False)
+    created_at = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+
+    user = db.relationship('User')
+
